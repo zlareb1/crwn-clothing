2067e40792b7e4a6835ffd6ee1b600657985af8f
mzlareb <mohammadlareb.zafar@nutanix.com>
2020-01-01 12:01:02 +0530

    updated

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_cP77Iv9CZrIIiHWJlOosSuKh00rFXAcxpl';
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
     console.log(token);
